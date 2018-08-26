be3ff44eea34bd29b63659163748a2655669739a
dumitrurusu <rusudumitru1994@gmail.com>
2018-08-26 17:11:37 +0300

    test change

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ salut: "costel" });
 });
 
 const PORT = process.env.PORT || 5000;
